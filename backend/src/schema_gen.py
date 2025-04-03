5544a001a755b6c4e3b77500dec9a03d693aaa4e
root <root@danhvuive>
2025-04-03 20:07:33 +0700

    add user schema

--- backend/src/schema_gen.py
+++ backend/src/schema_gen.py
@@ -44,7 +44,57 @@ def create_schema():
         }
     }
 
-    # Pass the schema directly to the validator parameter
+    user_schema = {
+        "$jsonSchema": {
+            "bsonType": "object",
+            "required": ["userId", "username","email","role", "faceID", "password", "createdAt"],
+            "properties": {
+                "userId": {
+                    "bsonType": "string",
+                    "description": "must be a string and is required"
+                },
+                "gender": {
+                    "bsonType": "string",
+                    "description": "must be a string and is not required"
+                },
+                "username": {
+                    "bsonType": "string",
+                    "description": "must be a string and is required"
+                },
+                "email": {
+                    "bsonType": "string",
+                    "description": "must be a string and is required"
+                },
+                "role": {
+                    "bsonType": "string",
+                    "description": "must be a string and is required"
+                },
+                "faceID": {
+                    "bsonType": "string",
+                    "description": "must be a string (hash or encoded) and is required"
+                },
+                "password": {
+                    "bsonType": "string",
+                    "description": "must be a string (hash or profile data) and is required"
+                },
+                "authorizedDevices": {
+                    "bsonType": "array",
+                    "items": {"bsonType": "string"},
+                    "description": "must be an array of device IDs (strings)"
+                },
+                "createdAt": {
+                    "bsonType": "date",
+                    "description": "must be a date and is required"
+                }
+            }
+        }
+    }
+
+    db.create_collection(
+        'user', 
+        validator=user_schema
+    )
+    # Pass the schema directly to the validator paramer
     db.create_collection(
         "record_device",
         validator=device_schema
